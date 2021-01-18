0cccdc6597538d66a244faf4b0251ce6a272c337
Youssef Touggani <yousseftouggani@youss.home>
2021-01-18 10:34:16 +0100

    config

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,5 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://tobedefined.io/api"
+    ? "https://devops-tpfinal-back.herokuapp.com/api"
     : "http://localhost:2020/api";
 export default API_URL;
